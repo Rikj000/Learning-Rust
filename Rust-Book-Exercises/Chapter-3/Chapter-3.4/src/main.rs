9577fa359efe4c7820ed052bbd56055d358fc794
Rik Helsen <rik_helsen@hotmail.com>
2022-04-10 16:27:30 +0200

    ✨ Added Rust Book `Chapter 3.4`

---
+++ Rust-Book-Exercises/Chapter-3/Chapter-3.4/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // So we’re doing something complicated here, long enough that we need
+    // multiple lines of comments to do it! Whew! Hopefully, this comment will
+    // explain what’s going on.
+
+    // I’m feeling lucky today
+    let _lucky_number = 7; // I’m feeling lucky today
+}
