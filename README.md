16ff0568f27a49dc7af5ad0dca12e1005215b5a8
Rikj000 <rik_helsen@hotmail.com>
2023-03-11 15:17:20 +0100

    📝 Docs - Documented Iconomi `ref`

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
         <img src="https://img.shields.io/badge/Docs-Learning_Rust-blue?logo=libreoffice&logoColor=white" alt="The current place where you can find all Learning-Rust Documentation!">
     </a> <a href="https://github.com/Rikj000/Learning-Rust/blob/master/LICENSE">
         <img src="https://img.shields.io/github/license/Rikj000/Learning-Rust?label=License&logo=gnu" alt="GNU General Public License">
-    </a> <a href="https://www.iconomi.com/register?ref=JdFzz">
+    </a> <a href="https://www.iconomi.com/register?ref=zQQPK">
         <img src="https://img.shields.io/badge/Join-ICONOMI-blue?logo=bitcoin&logoColor=white" alt="ICONOMI - The world’s largest crypto strategy provider">
     </a> <a href="https://www.buymeacoffee.com/Rikj000">
         <img src="https://img.shields.io/badge/-Buy%20me%20a%20Coffee!-FFDD00?logo=buy-me-a-coffee&logoColor=black" alt="Buy me a Coffee as a way to sponsor this project!">
--- Rust-Book-Exercises/Chapter-10/Chapter-10.1/src/main.rs
+++ Rust-Book-Exercises/Chapter-10/Chapter-10.1/src/main.rs
@@ -1,31 +1,3 @@
 fn main() {
-    // Fetch the largest number in the list
-    let number_list1 = vec![34, 50, 25, 100, 65];
-    let mut largest1 = number_list1[0];
-
-    for number in number_list1 {
-        if number > largest1 { largest1 = number; }
-    }
-
-    let result1 = largest1;
-    println!("The largest number is {}", result1);
-
-    // Fetch the largest number in the list through code abstraction to a new function
-    let number_list2 = vec![34, 50, 25, 100, 65];
-    let result2 = largest(&number_list2);
-    println!("The largest number is {}", result2);
-
-    let number_list3 = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let result3 = largest(&number_list3);
-    println!("The largest number is {}", result3);
-}
-
-fn largest(list: &[i32]) -> i32 {
-    let mut largest = list[0];
-
-    for &item in list {
-        if item > largest { largest = item; }
-    }
-
-    largest
+    println!("Hello, world!");
 }
